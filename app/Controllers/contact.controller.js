c00b9bde859876e3862dc506d7a7809c64435ea4
TranMinhKhangg <117838042+TranMinhKhangg@users.noreply.github.com>
2025-02-26 23:17:18 +0700

    Update contact.controller.js

--- app/Controllers/contact.controller.js
+++ app/Controllers/contact.controller.js
@@ -1,24 +1,3 @@
-exports.create = (req, res) => {
-    res.send({message: "Create handler" });
-};
-exports.findAll = (req, res) => {
-    res.send({message:"findAll handler" });
-};
-exports.findOne = (req, res) => {
-    res.send({message:"findOne handler" });
-};
-exports.update = (req, res) => {
-    res.send({message:"update handler" });
-};
-exports.delete = (req, res) => {
-    res.send({message:"delete handler" });
-};
-exports.deleteAll = (req, res) => {
-    res.send({message:"deleteAll handler" });
-};
-exports.findAllFavorite = (req, res) => {
-    res.send({message:"findAllFavorite handler" });
-};
 
 const ApiError = require("../api-error");
 const ContactService = require("../services/contact.service");
