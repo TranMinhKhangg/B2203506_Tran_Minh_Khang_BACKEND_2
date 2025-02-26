001645e1d0fd1b5527ae4ed7c21d4aa74d7538e9
TranMinhKhangg <117838042+TranMinhKhangg@users.noreply.github.com>
2025-02-26 23:23:46 +0700

    Cai dat cac handler va truy xuat co so du lieu

--- app/Controllers/contact.controller.js
+++ app/Controllers/contact.controller.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-// exports.create = (req, res) => {
-//     res.send({message: "Create handler" });
-// };
-// exports.findAll = (req, res) => {
-//     res.send({message:"findAll handler" });
-// };
-// exports.findOne = (req, res) => {
-//     res.send({message:"findOne handler" });
-// };
-// exports.update = (req, res) => {
-//     res.send({message:"update handler" });
-// };
-// exports.delete = (req, res) => {
-//     res.send({message:"delete handler" });
-// };
-// exports.deleteAll = (req, res) => {
-//     res.send({message:"deleteAll handler" });
-// };
-// exports.findAllFavorite = (req, res) => {
-//     res.send({message:"findAllFavorite handler" });
-// };
-=======
->>>>>>> c00b9bde859876e3862dc506d7a7809c64435ea4
-
 const ApiError = require("../api-error");
 const ContactService = require("../services/contact.service");
 const MongoDB = require("../Utils/mongodb.util");
